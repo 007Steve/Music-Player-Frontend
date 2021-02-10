48f093ca06324d79a3c8514e43fea7ae4030354f
007steve <stephen.plummer1.sp@gmail.com>
2021-02-10 14:55:48 -0500

    add home page

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+function Home() {
+    return (
+        <div className="home">
+            <Sidebar/>
+        </div>
+    )
+}
+
+export default Home
