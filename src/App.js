af2cdda7c75f9f579189df82582762fd22c89b75
007steve <stephen.plummer1.sp@gmail.com>
2021-02-12 17:07:04 -0500

    updated add fetch redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import './App.css';
-
+import React, { useEffect } from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import { setCurrentSong } from "./features/currentSongSlice";
+import { songs, selectSongs } from "./features/songsSlice";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 function App() {
+
+  const currentSongS = useSelector(selectSongs);
+  useEffect(() => {
+    fetch('https://api.lyrics.ovh/v1/logic/amen')
+    .then(response => response.json())
+    .then(data => console.log(data));
+  }, [])
+
+  //DISPATCH
+  const dispatch = useDispatch();
+// add song data and redux
+  useEffect(() => {
+    fetch("http://localhost:5000/api/songs")
+      .then((response) => response.json())
+      .then((data) => dispatch(songs(data)));
+  }, []);
+  //DISPATCH CURRENT SONG
+  dispatch(
+    setCurrentSong({
+      setCurrentSong: currentSongS,
+    })
+  );
   return (
     <div className="App">
-
+      <Home />
     </div>
   );
 }
