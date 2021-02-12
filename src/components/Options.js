f41f623678023d0595a96bd276ddef6b01f143bc
007steve <stephen.plummer1.sp@gmail.com>
2021-02-12 17:02:04 -0500

    add newCurrentSong

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import '../styles/Options.css'
-import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import React from "react";
+import "../styles/Options.css";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import {
+  selectcurrentSong,
+  setCurrentSong,
+} from "../features/currentSongSlice";
+import { useSelector } from "react-redux";
 function Options() {
-    return (
-        <div className="options">
-            <button className="options__btn">Lyrics</button>
-            <MoreHorizIcon className="options__icon"/>
-        </div>
-    )
+  const newCurrentSong = useSelector(selectcurrentSong);
+  return (
+    <div className="options">
+      <button
+        className={`options__btn ${newCurrentSong.setCurrentSong.artist}`}
+      >
+        Lyrics
+      </button>
+      <MoreHorizIcon className="options__icon" />
+    </div>
+  );
 }
 
-export default Options
+export default Options;
