0b737ed18f190e67dffb22a39b3458da720bae0d
007steve <stephen.plummer1.sp@gmail.com>
2021-02-12 16:52:19 -0500

    add setCurrentSong to dispatch redux

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,24 +2,25 @@ import React from "react";
 import "../styles/Card.css";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import { useDispatch } from "react-redux";
-// import { setCurrentSong } from "../features/currentSongSlice";
-function Card({ artist, time, image, album, song }) {
+import { setCurrentSong } from "../features/currentSongSlice";
+function Card({ song }) {
+  // add song
   const dispatch = useDispatch();
-  // const selectedSongHandler = () => {
-  //   dispatch(
-  //     setCurrentSong({
-  //       setCurrentSong: song,
-  //     })
-  //   );
-  // };
+  const selectedSongHandler = () => {
+    dispatch(
+      setCurrentSong({
+        setCurrentSong: song,
+      })
+    );
+  };
   return (
-    <div className="card" onClick>
-      <img className="card-image" src={image} alt="" />
+    <div className="card" onClick={selectedSongHandler}>
+      <img className="card-image" src={song.cover} alt="" />
       <div className="card-container">
-        <h4 className="card-album">{album}</h4>
-        <p className="card-artist">{artist}</p>
-        <p className="card-time">{time}</p>
-        {/* <FavoriteIcon className="card-like" /> */}
+        <h4 className="card-album">{song.artist}</h4>
+        <p className="card-artist">{song.song} </p>
+        <p className="card-time">{song.time}</p>
+        <FavoriteIcon className="card-like" />
       </div>
     </div>
   );
