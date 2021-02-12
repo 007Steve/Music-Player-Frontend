bff162b259948c63c198bd86a5af37ebbd28a1e0
007steve <stephen.plummer1.sp@gmail.com>
2021-02-12 16:55:55 -0500

    add Song to map info

--- src/components/Container.js
+++ src/components/Container.js
@@ -4,7 +4,11 @@ import Banner from "./Banner";
 import Card from "./Card";
 import Options from "./Options";
 import Lyrics from "./Lyrics";
+import { selectSongs } from "../features/songsSlice";
+import { useSelector } from "react-redux";
 function Container() {
+  //added songs and selector
+  const songs = useSelector(selectSongs);
   return (
     <div className="container">
       <Banner />
@@ -20,55 +24,10 @@ function Container() {
             album="Logic"
             song=""
           />
-          <Card
-            artist="Amen"
-            time="3:04"
-            image="https://upload.wikimedia.org/wikipedia/en/4/4c/Logic_No_Pressure_album_cover.jpeg"
-            album="Logic"
-            song=""
-          />
-          <Card
-            artist="Amen"
-            time="3:04"
-            image="https://upload.wikimedia.org/wikipedia/en/4/4c/Logic_No_Pressure_album_cover.jpeg"
-            album="Logic"
-            song=""
-          />
-          <Card
-            artist="Amen"
-            time="3:04"
-            image="https://upload.wikimedia.org/wikipedia/en/4/4c/Logic_No_Pressure_album_cover.jpeg"
-            album="Logic"
-            song=""
-          />
-          <Card
-            artist="Amen"
-            time="3:04"
-            image="https://upload.wikimedia.org/wikipedia/en/4/4c/Logic_No_Pressure_album_cover.jpeg"
-            album="Logic"
-            song=""
-          />
-          <Card
-            artist="Amen"
-            time="3:04"
-            image="https://upload.wikimedia.org/wikipedia/en/4/4c/Logic_No_Pressure_album_cover.jpeg"
-            album="Logic"
-            song=""
-          />
-          <Card
-            artist="Amen"
-            time="3:04"
-            image="https://upload.wikimedia.org/wikipedia/en/4/4c/Logic_No_Pressure_album_cover.jpeg"
-            album="Logic"
-            song=""
-          />
-          <Card
-            artist="Amen"
-            time="3:04"
-            image="https://upload.wikimedia.org/wikipedia/en/4/4c/Logic_No_Pressure_album_cover.jpeg"
-            album="Logic"
-            song=""
-          />
+
+          {songs.map((song) => (
+            <Card key={song.id} song={song} />
+          ))}
         </div>
       )}
     </div>
