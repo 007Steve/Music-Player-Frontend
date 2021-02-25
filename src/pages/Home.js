943dd6a7779b97b5f39f6c2ba64e136a55492f88
007steve <stephen.plummer1.sp@gmail.com>
2021-02-25 11:30:26 -0500

    updated added player and container

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,20 @@
-import React from 'react'
-import Sidebar from '../components/Sidebar'
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import Player from "../components/Player";
+import Container from "../components/Container";
+import "../styles/Home.css";
+
 function Home() {
-    return (
-        <div className="home">
-            <Sidebar/>
-        </div>
-    )
+  return (
+    <div className="home">
+      <div className="home__container">
+        <Sidebar />
+
+        <Container />
+      </div>
+      <Player />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
