6f5b1c5b7bd83fac647415bf68d0f5eaabf9e420
007steve <stephen.plummer1.sp@gmail.com>
2021-02-26 16:42:05 -0500

    updated  songs array

---
+++ src/SongsData.js
@@ -0,0 +1,67 @@
+function music() {
+    return  [
+        {
+            id: "1",
+            song: "Beaver Creek",
+            artist: "Aso, Middle School, Aviino",
+            cover: "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+            color: ["#205950", "#2ab3bf"],
+            active: true,
+            position: "first",
+        },
+        {
+            id: "2",
+            song: "Daylight",
+            artist: "Aiguille",
+            cover: "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
+            color: ["#EF8EA9", "#ab417f"],
+            active: false,
+            position: "second",
+        },
+        {
+            id: "3",
+            song: "Keep Going",
+            artist: "Swørn",
+            cover: "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
+            color: ["#CD607D", "#c94043"],
+            active: false,
+            position: "third",
+        },
+        {
+            id: "4",
+            song: "Beaver Creek",
+            artist: "Aso, Middle School, Aviino",
+            cover: "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+            color: ["#205950", "#2ab3bf"],
+            active: true,
+            position: "first",
+        },
+        {
+            id: "5",
+            song: "Daylight",
+            artist: "Aiguille",
+            cover: "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
+            color: ["#EF8EA9", "#ab417f"],
+            active: false,
+            position: "second",
+        },
+        {
+            id: "6",
+            song: "Keep Going",
+            artist: "Swørn",
+            cover: "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
+            color: ["#CD607D", "#c94043"],
+            active: false,
+            position: "third",
+        },
+       
+    ]
+}
+
+export default music
