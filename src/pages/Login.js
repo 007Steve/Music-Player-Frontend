c5a42d8841f25de5fe1bd70cfc0e27c0566548c1
007steve <stephen.plummer1.sp@gmail.com>
2021-02-26 16:21:04 -0500

    add link to signup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import "../styles/Login.css";
+import "../styles/Form.css";
 import axios from "axios";
+
 function Login() {
-//State
+  //State
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const history = useHistory();
-// Login
+  // Login Data
   const login = async (e) => {
     e.preventDefault();
 
@@ -30,33 +32,39 @@ function Login() {
     history.push("/dashboard");
   };
   return (
-    <div className="form__div">
-      <form action="" onSubmit={login} className="form">
-        <h1 className="form__title">Login</h1>
-        <h4 className="form__error">{errorMessage}</h4>
-        <label htmlFor="Email"></label>
-        <input
-          className="form__input"
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label htmlFor="Password"></label>
-        <input
-          className="form__input"
-          type="text"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit" className="btn-grad">
-          Login
-        </button>
-      </form>
+    <div className="login">
+      <div className="form__div">
+        <form action="" onSubmit={login} className="form">
+          <h1 className="form__title">Login</h1>
+          <h4 className="form__error">{errorMessage}</h4>
+          <label htmlFor="Email"></label>
+          <input
+            className="form__input"
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="Password"></label>
+          <input
+            className="form__input"
+            type="password"
+            placeholder="Password"
+            autoComplete="true"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" className="btn-grad">
+            Login
+          </button>
+          <p className="form__link">
+            Don't have an
+            <Link to="/signup"> account?</Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 }
 
 export default Login;
-
